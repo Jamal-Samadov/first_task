3f533a4615c428325e85ab8ee17ceb4027c7214f
Jamaltscode <jamalts@code.edu.az>
2022-04-13 10:33:17 +0400

    new taskk

--- task2.cs
+++ task2.cs
@@ -1,5 +1,21 @@
-﻿Build started...
-1>------ Build started: Project: task2, Configuration: Debug Any CPU ------
-1>Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
-1>task2 -> C:\Users\User\source\repos\task2\task2\bin\Debug\netcoreapp3.1\task2.dll
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string task = "5 < 9";
+            Console.WriteLine(task);
+
+            int number = 5;
+            byte number2 = 9;
+
+            bool result = number < number2;
+
+            Console.WriteLine(result);
+        }
+    }
+    }
+
