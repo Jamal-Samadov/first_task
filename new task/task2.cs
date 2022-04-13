20000a0d35676acc83eb1571f222880573bc4574
Jamaltscode <102617640+Jamaltscode@users.noreply.github.com>
2022-04-13 10:34:48 +0400

    Add files via upload

---
+++ new task/task2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string task = "5 < 9";
+            Console.WriteLine(task);
+
+            int number = 5;
+            byte number2 = 9;
+
+            bool result = number < number2;
+
+            Console.WriteLine(result);
+        }
+    }
+    }
+
