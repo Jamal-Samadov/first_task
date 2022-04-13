3f958cc65d73fffbcd9a1b8d5a1531c1b0e747bb
Jamaltscode <jamalts@code.edu.az>
2022-04-13 10:27:12 +0400

    new task uploaded

---
+++ Task1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 5;
+            byte number2 = 9;
+
+            bool result = number > number2;
+
+            Console.WriteLine(number = number2);
+        }
+    }
+}
