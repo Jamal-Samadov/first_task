532d9c7485a74c57b1efebc2b8ccd614d9ef0ec2
Jamaltscode <102617640+Jamaltscode@users.noreply.github.com>
2022-04-13 12:20:34 +0400

    Add files via upload

---
+++ main task/FirstTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string simple = "tek eded";
+            byte number = 7;
+
+                Console.WriteLine("tek eded = " +  number);
+            
+            bool result = true;
+            
+            Console.WriteLine(result); 
+        }
+    }
+}
