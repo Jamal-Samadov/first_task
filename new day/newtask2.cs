1b3ab0bc27e5e35747e4fffc2ed5dd18ae12f759
Jamaltscode <102617640+Jamaltscode@users.noreply.github.com>
2022-04-14 10:57:39 +0400

    Add files via upload

---
+++ new day/newtask2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Vugar", "Camal", "Mirzaur", "Salamtcilig", "Beli", "Dunyamali" };
+
+
+
+            foreach(string name in names)
+            {
+                int counter = 0;
+                foreach (char ch in name)
+                {
+                    counter++;
+                }
+                
+                if (counter > 5 )
+                {
+                    Console.WriteLine(name + " --> " + counter + " herf");
+                }
+            }
+        }
+    }
+}
